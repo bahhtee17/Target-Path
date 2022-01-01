6c29bde3561e51ee6366f33d3fbe218c538418ad
Baxtiyor <baxtiyorshukurjanov36@gmail.com>
2022-01-01 21:02:56 +0500

    form binded and ready for output

--- src/components/Input/InputItem.js
+++ src/components/Input/InputItem.js
@@ -1,24 +1,46 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 
 const InputItem = () => {
   const [inputText, setInputText] = useState("");
   const [selectCategory, setSelectCategory] = useState("");
   const [inputDate, setInputDate] = useState("");
 
+  const inputChangeHandler = (event) => {
+    setInputText(event.target.value);
+    console.log(inputText);
+  };
+
+  const dateChangeHandler = (event) => {
+    setInputDate(event.target.value);
+    console.log(inputDate);
+  };
+
+  const selectChangeHandler = (event) => {
+    setSelectCategory(event.target.value);
+  };
+
   return (
     <form>
       <div>
-        <input type='text' placeholder='Add Target' />
+        <input
+          onChange={inputChangeHandler}
+          value={inputText}
+          type='text'
+          placeholder='Add Target'
+        />
+        {inputText}
       </div>
       <div>
-        <select>
-          <option>Must</option>
-          <option>Will</option>
-          <option>Might</option>
+        <select onChange={selectChangeHandler} value={selectCategory}>
+          <option value='Must'>Must</option>
+          <option value='Will'>Will</option>
+          <option value='Might'>Might</option>
         </select>
+        {selectCategory}
       </div>
       <div>
-        <input type='date' />
+        <input onChange={dateChangeHandler} value={inputDate} type='date' />
+        {inputDate}
       </div>
       <div>
         <button type='submit'>Add Target</button>
